ac816de36feac53ca112d63b6922ab2def67ec99
Daniel Sheeler <six600110@pobox.com>
2014-02-13 02:57:13 -0600

    adding clutter (though code is commented); add way to get rid of [Explicit]s from titles

--- Makefile
+++ Makefile
@@ -4,26 +4,32 @@ all: $(PROGS)
 
 CC = g++
 
-LFLAGS = -lFestival -lestools -lestbase -leststring
-LFLAGS += 
-LFLAGS += -ljack -lespeak -lpthread -lglib-2.0 -lxmmsclient++-glib -lxmmsclient++ -lxmmsclient -lxmmsclient-glib
-LFLAGS +=  $(shell pkg-config --libs gtkmm-3.0) $(shell pkg-config --libs libnotify)
+LFLAGS = -lFestival -lestools -lestbase -leststring -lclutter-1.0 -lcogl-pango\
+				 -lcogl -ljack -lpthread -lglib-2.0 -lxmmsclient++-glib \
+         -lxmmsclient++ -lxmmsclient -lxmmsclient-glib \
+         $(shell pkg-config --libs gtkmm-3.0) $(shell pkg-config --libs libnotify) \
+				 -lncurses
 
-CFLAGS = -Wall -c -I -I /usr/local/include/xmms2 -I /usr/include/glib-2.0 -I /usr/lib/x86_64-linux-gnu/glib-2.0/include 
-CFLAGS +=  -I/usr/include/libnotify $(shell pkg-config --cflags gtkmm-3.0) $(shell pkg-config --cflags libnotify)
-CFLAGS += -I/usr/include/festival -I/usr/include/speech_tools -I/usr/local/include/xmms2
-LIBS = 
+CFLAGS = -Wall -c -I /usr/include/xmms2 -I /usr/include/glib-2.0 \
+				 -I /usr/lib/x86_64-linux-gnu/glib-2.0/include \
+				 -I/usr/include/libnotify $(shell pkg-config --cflags gtkmm-3.0) \
+				 $(shell pkg-config --cflags libnotify) \
+				 -I/usr/include/festival -I/usr/include/speech_tools \
+				 -I/usr/local/include -I/usr/include/cogl -I/usr/include/clutter-1.0 \
+         -I/usr/include/json-glib-1.0
+
+LIBS =
 
 SRCS = digitalDJ.cpp
 OBJS = $(SRCS:.cpp=.o)
-HDRS = 
+HDRS =
 
 .SUFFIXES:
 
 .SUFFIXES: .cpp
 
 %.o : %.cpp
-	$(CC) ${CFLAGS} $<	
+	$(CC) ${CFLAGS} $<
 
 digitalDJ: ${OBJS}
 	${CC} -o $@ ${OBJS} ${LFLAGS}
--- digitalDJ.cpp
+++ digitalDJ.cpp
@@ -5,7 +5,6 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include <espeak/speak_lib.h>
 #include <jack/jack.h>
 #include <jack/ringbuffer.h>
 #include <xmmsclient/xmmsclient++.h>
@@ -19,6 +18,10 @@
 #include <gdkmm-2.4/gdkmm/pixbuf.h>
 #include <festival.h>
 
+#include <cogl/cogl.h>
+#include <cogl-pango/cogl-pango.h>
+#include <clutter/clutter.h>
+
 #include <cstdlib>
 #include <string>
 #include <iostream>
@@ -27,6 +30,38 @@
 #define ICON_PATH "/usr/local/share/icons/hicolor/64x64/apps/xmms2.png"
 
 typedef jack_default_audio_sample_t sample_t;
+
+#define SHADOW_X_OFFSET         3
+#define SHADOW_Y_OFFSET         3
+
+static void
+_text_paint_cb (ClutterActor *actor)
+{
+  PangoLayout *layout;
+  guint8 real_opacity;
+  CoglColor color;
+  ClutterText *text = CLUTTER_TEXT (actor);
+  ClutterColor text_color = { 0, };
+
+  /* Get the PangoLayout that the Text actor is going to paint */
+  layout = clutter_text_get_layout (text);
+
+  /* Get the color of the text, to extract the alpha component */
+  clutter_text_get_color (text, &text_color);
+
+  /* Composite the opacity so that the shadow is correctly blended */
+  real_opacity = clutter_actor_get_paint_opacity (actor)
+               * text_color.alpha
+               / 255;
+
+  /* Create a #ccc color and premultiply it */
+  cogl_color_init_from_4ub (&color, 0x20, 0x90, 0x180, real_opacity);
+  cogl_color_premultiply (&color);
+
+  /* Finally, render the Text layout at a given offset using the color */
+  cogl_pango_render_layout (layout, SHADOW_X_OFFSET, SHADOW_Y_OFFSET, &color, 0);
+}
+
 
 NotifyNotification *notification;
 
@@ -37,11 +72,11 @@ jack_ringbuffer_t *jringbuf;
 const char *err_buf;
 int intval;
 
-void log (const char *pattern, ...) 
+void log (const char *pattern, ...)
 {
 	va_list arguments;
 	FILE *logfile;
-	
+
 	logfile = fopen ("/tmp/DDJ.log", "a");
 	fprintf (logfile, pattern);
 	fclose(logfile);
@@ -71,8 +106,15 @@ static void signal_handler(int sig) {
 }
 
 void festival_Synth(const char *text_to_say) {
+  /*Strip out annoying [Explicit] from title.*/
+  std::string edited_text(text_to_say);
+  std::size_t found = edited_text.find("[Explicit]");
+  if (found != edited_text.npos) {
+    edited_text.erase(found, 10);
+  }
+
   EST_Wave wave;
-  festival_text_to_wave(text_to_say, wave);
+  festival_text_to_wave(edited_text.c_str(), wave);
   double scale = 1/32768.0;
   wave.resample(48000);
 
@@ -97,72 +139,33 @@ void festival_Synth(const char *text_to_say) {
   } while (num_bytes_to_write > 0);
 }
 
-int synth_callback(short *wav, int numsamples, espeak_EVENT *events) {
-
-	size_t num_bytes_to_write;
-	sample_t buf[2*numsamples];
- 
-	if (!wav) return 0;
-	if (numsamples == 0) return 0;
-
-	/* i am assuming espeak output at 22050 hz and jack at 44100 hz.
-	 this is true for me and i cannot find a way to
-	 modify espeak sample rate */
-	
-	double scale = 1/32768.0;
-	
-	for (int i = 0; i < numsamples; i++) {
-		buf[2*i] = wav[i] * scale;
-	}
-	
-	for (int i = 1; i < 2 * numsamples - 2; i += 2) {
-		buf[i] = 0.5 * (buf[i-1] + buf[i+1]);
-	}
-	
-	buf[2*numsamples - 1] = 2 * buf[2*numsamples-2] - buf[2*numsamples-3];
-	
-	num_bytes_to_write = 2*numsamples*sizeof(sample_t);
-	
-	do {
-		int nwritten = jack_ringbuffer_write(jringbuf, (char*)buf, num_bytes_to_write);
-		
-		if (nwritten < num_bytes_to_write && num_bytes_to_write > 0) {
-			usleep(100000);
-		}
-		num_bytes_to_write -= nwritten;
-	
-	} while (num_bytes_to_write > 0);
-	
-	return 0;
-}
-
 /**
  * The process callback for this JACK application is called in a
  * special realtime thread once for each audio cycle.
  *
  */
 
-int process (jack_nframes_t nframes, void *arg) 
+int process (jack_nframes_t nframes, void *arg)
 {
   sample_t *out[2];
 	size_t num_bytes_to_write, num_bytes_written, num_samples_written;
 	int i;
-	
+
 	num_bytes_to_write = sizeof(sample_t) * nframes;
-	
+
 	for (i = 0; i < 2; i++) {
 		out[i] = (sample_t *) jack_port_get_buffer (my_output_ports[i], nframes);
 	}
-	
+
 	num_bytes_written = jack_ringbuffer_read(jringbuf, (char*)out[0], num_bytes_to_write);
 	num_samples_written = num_bytes_written / sizeof(sample_t);
 	for (i = 0; i < num_samples_written; i++) {
 		out[1][i] = out[0][i];
-	} 
+	}
 	for (i = num_samples_written; i < nframes; i++) {
 		out[0][i] = out[1][i] = 0.0;
 	}
-	return 0;      
+	return 0;
 }
 
 
@@ -175,7 +178,7 @@ class DDJXmms2Client {
     bool my_current_id(const int& id);
     bool error_handler(const std::string& error);
     bool handle_bindata(const Xmms::bin& data);
-                        
+
   private:
     Xmms::Client client_;
     Xmms::Client sync_client_;
@@ -188,9 +191,9 @@ DDJXmms2Client::DDJXmms2Client()
   client_.connect(std::getenv("XMMS_PATH"));
   sync_client_.connect(std::getenv("XMMS_PATH"));
 
-  client_.playback.broadcastCurrentID()( 
+  client_.playback.broadcastCurrentID()(
    Xmms::bind( &DDJXmms2Client::my_current_id, this ),
-   Xmms::bind( &DDJXmms2Client::error_handler, this ) 
+   Xmms::bind( &DDJXmms2Client::error_handler, this )
   );
 
   client_.setMainloop( new Xmms::GMainloop( client_.getConnection() ) );
@@ -199,9 +202,9 @@ DDJXmms2Client::DDJXmms2Client()
   * Initialize and run glib mainloop, check out glib documentation
   * for this.
   */
-  GMainLoop* ml = g_main_loop_new( 0, 0 );	
+  GMainLoop* ml = g_main_loop_new( 0, 0 );
   g_main_loop_run( ml );
-  
+
 }
 
 DDJXmms2Client::~DDJXmms2Client() { }
@@ -213,7 +216,7 @@ bool DDJXmms2Client::handle_bindata(const Xmms::bin& data) {
   try {
     Glib::RefPtr<Gdk::PixbufLoader> loader = Gdk::PixbufLoader::create();
     std::cout << data.size() << " is size of bindata." << std::endl;
-    
+
     loader->write( data.c_str(), data.size() );
     loader->close();
     image = scale_pixbuf( loader->get_pixbuf() );
@@ -223,13 +226,15 @@ bool DDJXmms2Client::handle_bindata(const Xmms::bin& data) {
     std::clog << "Could not load image: " << e.what() << std::endl;
   }
 
+  GError *gerr = 0;
+  notify_notification_show (notification, &gerr);
+
+
   return true;
 }
 
 bool DDJXmms2Client::my_current_id(const int& id) {
 
-  uint espeak_id;
-  
   std::string val, say, msg;
 
   msg += "xmms2d: ";
@@ -242,7 +247,7 @@ bool DDJXmms2Client::my_current_id(const int& id) {
     try {
       std::cout << info["artist"] << std::endl;
       val = boost::get< std::string >(info["artist"]) + std::string(". ");
-   
+
     } catch( Xmms::no_such_key_error& err ) {
 
       std::cout << "No artist" << std::endl;
@@ -268,14 +273,14 @@ bool DDJXmms2Client::my_current_id(const int& id) {
 
     try {
       val = boost::get< std::string >(info["picture_front"]);
-      client_.bindata.retrieve(val)( 
+      client_.bindata.retrieve(val)(
        Xmms::bind( &DDJXmms2Client::handle_bindata, this ),
-       Xmms::bind( &DDJXmms2Client::error_handler, this ) 
+       Xmms::bind( &DDJXmms2Client::error_handler, this )
        );
     }
     catch(Xmms::no_such_key_error& err) {
     }
-    
+
   }
   catch( Xmms::result_error& err ) {
     // This can happen if the id is not in the medialib
@@ -299,22 +304,10 @@ bool DDJXmms2Client::my_current_id(const int& id) {
   }
 
 */
-  GError *gerr = 0;
   notify_notification_update (notification, msg.c_str(), NULL, NULL);
-  notify_notification_show (notification, &gerr);
-
   jack_ringbuffer_reset(jringbuf);
 
   festival_Synth(say.c_str());
-  /*espeak_Synth(say.c_str(),
-   8000,
-   0,
-   POS_CHARACTER,
-   0,
-   espeakCHARS_AUTO,
-   &espeak_id,
-   NULL);*/
-
   return true;
 
 }
@@ -341,7 +334,7 @@ void setup_jack() {
   client_name = (char *) malloc(80 * sizeof(char));
 
   char *tmp = "DigitalDJ";
-  
+
   strcpy(client_name, tmp);
   client = jack_client_open (client_name, jack_options, &status);
   if (client == NULL) {
@@ -392,28 +385,7 @@ void setup_festival() {
 
   festival_eval_command("(voice_cmu_us_slt_arctic_clunits)");
   festival_Synth("Hi.  I am your synthetic xmms2, jack DJ.  I hope you like my voice!");
-  
-}
 
-void setup_espeak() {
-  int buflength = 20;
-  espeak_Initialize(AUDIO_OUTPUT_RETRIEVAL,
-                    buflength, NULL, 0);
-
-  espeak_SetSynthCallback(synth_callback);
-  espeak_SetParameter(espeakVOLUME, 190, 0);
-  espeak_SetParameter(espeakWORDGAP, 5, 0);
-  espeak_SetParameter(espeakRATE, 76, 0);
-  espeak_SetParameter(espeakPITCH, 100, 0);
-  espeak_SetParameter(espeakRANGE, 200, 0);
-
-  espeak_VOICE voice_spec;
-  voice_spec.name = NULL;
-  voice_spec.languages = "english-us";
-  voice_spec.gender=1;
-  voice_spec.age=20;
-  voice_spec.variant = 3;
-  espeak_SetVoiceByProperties(&voice_spec);
 }
 
 void setup_signal_handler() {
@@ -424,13 +396,35 @@ void setup_signal_handler() {
 }
 
 int main (int argc, char *argv[]) {
+  /*ClutterActor *stage;
+  ClutterActor *text;
+
+  if (clutter_init (&argc, &argv) != CLUTTER_INIT_SUCCESS)
+    return 1;
+
+  stage = clutter_stage_new ();
+  clutter_stage_set_title (CLUTTER_STAGE (stage), "Text shadow");
+  g_signal_connect (stage, "destroy", G_CALLBACK (clutter_main_quit), NULL);
+
+  text = clutter_text_new ();
+  clutter_text_set_text (CLUTTER_TEXT (text), "Hello, World!");
+  clutter_text_set_font_name (CLUTTER_TEXT (text), "Sans 64px");
+  clutter_actor_add_constraint (text, clutter_align_constraint_new (stage, CLUTTER_ALIGN_X_AXIS, 0.5));
+  clutter_actor_add_constraint (text, clutter_align_constraint_new (stage, CLUTTER_ALIGN_Y_AXIS, 0.5));
+  g_signal_connect (text, "paint", G_CALLBACK (_text_paint_cb), NULL);
+
+  clutter_container_add (CLUTTER_CONTAINER (stage), text, NULL);
+
+  clutter_actor_show (stage);
+
+  clutter_main ();*/
+
 
   Gtk::Main kit( argc, argv );
 
   notify_init("xmms2-jack-dj");
   notification = notify_notification_new("", NULL, ICON_PATH);
   setup_jack();
-  setup_espeak();
   setup_festival();
   setup_signal_handler();
 
